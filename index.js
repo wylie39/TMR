0c32ab3a6288ade4092da51b657b5f56038d6ac7
wylie fowler <wylie@tradersabacus.com>
2021-02-28 10:01:57 -0500

    Make sure date formats are the same

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-exports.recommend = function(json) {
-  var date = new Date();
-  var array = json.sort()
-  var enabled_messages = array.filter(o => o.enabled === true);
+exports.recommend = function (json) {
+    var date = new Date();
+    var array = json.sort();
+    var enabled_messages = array.filter((o) => o.enabled === true);
 
-  function nomessage() {
-    var nodate_messages = enabled_messages.filter(o => o.air_date !== true);
-    return nodate_messages[Math.floor(Math.random() * nodate_messages.length)].message_text;
-  }
-
-
-  if (enabled_messages.length) {
-    var date_messages = enabled_messages.filter(o => o.air_date === true);
-    if (date_messages.length) {
-      var todays_messages = enabled_messages.filter(o => o.airdate === date.toLocaleDateString("en", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-      }));
-      if (todays_messages.length) {
-        return todays_messages[Math.floor(Math.random() * todays_messages.length)].message_text;
-
-      }else {
-        return nomessage();
-      }
-    } else {
-      return nomessage();
+    function nomessage() {
+        var nodate_messages = enabled_messages.filter(
+            (o) => o.air_date !== true
+        );
+        return nodate_messages[
+            Math.floor(Math.random() * nodate_messages.length)
+        ].message_text;
     }
-  } else {
-    throw new Error("No messages");
-  }
 
-
-}
+    if (enabled_messages.length) {
+        var date_messages = enabled_messages.filter((o) => o.air_date === true);
+        if (date_messages.length) {
+            var todays_messages = date_messages.filter(function (e) {
+                return (
+                    new Date(e.airdate).toLocaleDateString("en", {
+                        year: "numeric",
+                        month: "2-digit",
+                        day: "2-digit",
+                    }) ===
+                    date.toLocaleDateString("en", {
+                        year: "numeric",
+                        month: "2-digit",
+                        day: "2-digit",
+                    })
+                );
+            });
+            if (todays_messages.length) {
+                return todays_messages[
+                    Math.floor(Math.random() * todays_messages.length)
+                ].message_text;
+            } else {
+                return nomessage();
+            }
+        } else {
+            return nomessage();
+        }
+    } else {
+        throw new Error("No messages");
+    }
+};
